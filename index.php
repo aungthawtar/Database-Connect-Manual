427d0a2663d46f032836e78a5564cba06e0a4b81
Aung Thaw Tar <aungthawtar@Aungs-MacBook-Pro.local>
2020-07-19 12:59:35 +0630

    Database Manual Connect

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+    define ('DB_HOST' , 'localhost');
+    define ('DB_USER' , 'root');
+    define ('DB_PASS' , 'root');
+    define ('DB_NAME' , 'php_crud');
+    define ('DB_PORT' , '8889');
+
+    $conn = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME,DB_PORT);
+
+    if(mysqli_connect_error()){
+        die('DB Connect fail');
+    }
+
+    echo "Complete";
+?>
